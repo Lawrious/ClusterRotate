64c17094ba535b03287466f673d2eb55e2bc1168
Lawrious <lawrence.bilton@gmail.com>
2021-07-20 16:22:21 +0100

    Updated to v0.1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='causticpy',
-      version='0.3.2',
-      description='Caustic mass estimator for astrophysical systems',
-      url='http://github.com/giffordw/causticpy',
-      author='Dan Gifford (Modified by Lawrence Bilton)',
-      author_email='',
+setup(name='rotateclustpy',
+      version='0.1',
+      description='An algorithm to determine the presence of rotation in galaxy clusters',
+      url='http://github.com/Lawrious/ClusterRotate',
+      author='Lawrence Bilton',
+      author_email='lawrence.bilton@gmail.com',
       license='',
       install_requires=['numpy','scipy','cosmolopy','matplotlib','astlib'],
-      packages=['causticpy'],
+      packages=['rotateclustpy'],
       zip_safe=False)
